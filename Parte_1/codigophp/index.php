a364bacf6f2b4482adc4b76ac1fa8f1773da6c33
vjimcor955 <vjimcor955@g.educaand.es>
2024-01-22 09:33:43 +0100

    reiniciar formulario

--- Parte_1/codigophp/index.php
+++ Parte_1/codigophp/index.php
@@ -43,27 +43,27 @@
 
     <!-- Question 4 -->
 
-    <!-- <div class="question">
+    <div class="question">
       <p>4. En PHP, ¿qué bucle se utiliza para ejecutar un bloque de código un número especificado de veces?</p>
       <label><input type="radio" name="q4" value="a Answer"> a) Bucle for (Respuesta correcta)</label>
       <label><input type="radio" name="q4" value="b"> b) Bucle while</label>
       <label><input type="radio" name="q4" value="c"> c) Bucle do...while</label>
       <label><input type="radio" name="q4" value="d"> d) Bucle foreach</label>
-    </div> -->
+    </div>
 
     <!-- Question 5 -->
 
-    <!-- <div class="question">
+    <div class="question">
       <p>5. What is the result of 2 + 2 in PHP?</p>
       <label><input type="radio" name="q5" value="a"> a) fopen</label>
       <label><input type="radio" name="q5" value="b"> b) file_open</label>
       <label><input type="radio" name="q5" value="c"> c) open_file</label>
       <label><input type="radio" name="q5" value="d Answer"> d) Ninguna de las anteriores (Respuesta correcta)</label>
-    </div> -->
+    </div>
 
     <!-- Question 6 -->
 
-    <!-- <div class="question">
+    <div class="question">
       <p>6. ¿Cuál es el propósito de la superglobal `$_GET` en PHP?</p>
       <label><input type="radio" name="q6" value="a"> a) Recuperar datos de un formulario con el método
         POST</label>
@@ -71,21 +71,21 @@
       <label><input type="radio" name="q6" value="c Answer"> c) Recuperar datos de la cadena de consulta URL (Respuesta
         correcta)</label>
       <label><input type="radio" name="q6" value="d"> d) Definir constantes globales</label>
-    </div> -->
+    </div>
 
     <!-- Question 7 -->
 
-    <!-- <div class="question">
+    <div class="question">
       <p>7. ¿Cuál de los siguientes es un ejemplo de constante mágica de PHP?</p>
       <label><input type="radio" name="q7" value="a"> a) $this</label>
       <label><input type="radio" name="q7" value="b Answer"> b) __LINE__ (Respuesta correcta)</label>
       <label><input type="radio" name="q7" value="c"> c) $var</label>
       <label><input type="radio" name="q7" value="d"> d) functionName()</label>
-    </div> -->
+    </div>
 
     <!-- Question 8 -->
 
-    <!-- <div class="question">
+    <div class="question">
       <p>8. ¿Qué hace la función `include` en PHP?</p>
       <label><input type="radio" name="q8" value="a"> a) Ejecuta un bloque de código solo si una condición es
         verdadera</label>
@@ -93,29 +93,30 @@
         correcta)</label>
       <label><input type="radio" name="q8" value="c"> c) Define una nueva función</label>
       <label><input type="radio" name="q8" value="d"> d) Genera un número aleatorio</label>
-    </div> -->
+    </div>
 
     <!-- Question 9 -->
 
-    <!-- <div class="question">
+    <div class="question">
       <p>9. ¿En PHP, qué comprueba el operador `===`?</p>
       <label><input type="radio" name="q9" value="a Answer"> a) Igualdad (Respuesta correcta)</label>
       <label><input type="radio" name="q9" value="b"> b) Asignación</label>
       <label><input type="radio" name="q9" value="c"> c) Desigualdad</label>
       <label><input type="radio" name="q9" value="d"> d) Comparación</label>
-    </div> -->
+    </div>
 
     <!-- Question 10 -->
 
-    <!-- <div class="question">
+    <div class="question">
       <p>10. ¿Cuál de los siguientes se utiliza para crear un objeto en PHP?</p>
       <label><input type="radio" name="q10" value="a Answer"> a) new (Respuesta correcta)</label>
       <label><input type="radio" name="q10" value="b"> b) objeto</label>
       <label><input type="radio" name="q10" value="c"> c) crear</label>
       <label><input type="radio" name="q10" value="d"> d) instancia</label>
-    </div> -->
+    </div>
 
     <input type="submit" value="Submit">
+    <a href="?retake=true">Reintentar formulario</a>
   </form>
   <div class="resutlado">
     <?php
@@ -123,21 +124,21 @@
         $q1 = strval($_POST["q1"]);
         $q2 = strval($_POST["q2"]);
         $q3 = strval($_POST["q3"]);
-        // $q4 = strval($_POST["q4"]);
-        // $q5 = strval($_POST["q5"]);
-        // $q6 = strval($_POST["q6"]);
-        // $q7 = strval($_POST["q7"]);
-        // $q8 = strval($_POST["q8"]);
-        // $q9 = strval($_POST["q9"]);
-        // $q10 = strval($_POST["q10"]);
+        $q4 = strval($_POST["q4"]);
+        $q5 = strval($_POST["q5"]);
+        $q6 = strval($_POST["q6"]);
+        $q7 = strval($_POST["q7"]);
+        $q8 = strval($_POST["q8"]);
+        $q9 = strval($_POST["q9"]);
+        $q10 = strval($_POST["q10"]);
         $nota = 0;
-
-        $lista_respuestas = [$q1, $q2, $q3];
-        // $lista_respuestas = [$q1, $q2, $q3, $q4, $q5, $q6, $q7, $q8, $q9, $q10];
+        
+        $lista_respuestas = [$q1, $q2, $q3, $q4, $q5, $q6, $q7, $q8, $q9, $q10];
 
         for ($i = 0; $i < count($lista_respuestas); $i++) {
-          // PHP 8 para usar str_contains
-          // TODO: Levantar docker con PHP 8
+          if (empty($lista_respuestas[$i])) {
+            $lista_respuestas[$i] = "";
+          }
           if (str_contains($lista_respuestas[$i], 'Answer')) {
             echo "<p>- Pregunta " . ($i + 1) . ": Correcta</p>";
             $nota++;
