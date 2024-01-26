c784019e5cc7134f287b90bb434f1be0a1277895
vjimcor955 <vjimcor955@g.educaand.es>
2024-01-26 08:48:26 +0100

    cuestionarios y preguntas bbdd

---
+++ Parte_2/sql/cuestionarios-quizz_app.sql
@@ -0,0 +1,3 @@
+INSERT INTO `Cuestionario` (`quiz_id`, `title`, `description`, `created_at`) VALUES 
+('1', 'Cuestionario 1', 'Examen de PHP Básico', '2024-01-22 08:40:00.000000'),
+('2', 'Cuestionario 2', 'Examen de Desarrollo Web', '2024-01-22 09:00:00.000000')
